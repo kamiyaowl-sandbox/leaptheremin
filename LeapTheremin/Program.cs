0a6dd18c9c7e9f1c7466edc0eac9880b06290a74
Fumiya Katoh <katoh.numazu.ct@gmail.com>
2014-10-20 18:45:27 +0900

    ArduinoWaveGenのライブラリ化

--- LeapTheremin/Program.cs
+++ LeapTheremin/Program.cs
@@ -8,25 +8,19 @@ using System.Threading.Tasks;
 namespace ConsoleApplication1 {
 	class Program {
 		static void Main(string[] args) {
+			var gen = new ArduinoWaveGen();
 			Console.Write("Com PortName>");
-			var serial = new SerialPort() {
-				BaudRate = 115200,
-				PortName = Console.ReadLine()
-			};
-			serial.DataReceived += serial_DataReceived;
+			gen.PortName = Console.ReadLine();
+			gen.Open();
 
-			serial.Open();
+
 			while (true) {
 				Console.Write("Freq>");
 				var freq = int.Parse(Console.ReadLine());
 				Console.Write("Vol>");
 				var vol = int.Parse(Console.ReadLine());
 
-				var src = new byte[] {
-					 0x81, (byte)((freq >> 7) & 0x7f), (byte)(freq & 0x7f),//31~16383
-					 0x82, 0x0, (byte)((vol >> 1) & 0x7f)
-				 };
-				serial.Write(src, 0, src.Length);
+				gen.Update(freq, vol);
 			}
 		}
 
@@ -36,6 +30,56 @@ namespace ConsoleApplication1 {
 			int len = port.Read(buf, 0, 1024);
 			string s = new string(buf.Take(len).Select(x => (char)x).ToArray());
 			Console.WriteLine(s);
+		}
+	}
+
+	class ArduinoWaveGen : IDisposable {
+		private SerialPort serialPort;
+
+		public string PortName { get; set; }
+
+		public ArduinoWaveGen() { }
+
+		public void Open() {
+			Close();
+			serialPort = new SerialPort() {
+				BaudRate = 115200,
+				PortName = this.PortName
+			};
+			serialPort.Open();
+		}
+		public void Close() {
+			if (serialPort != null) {
+				if (serialPort.IsOpen) serialPort.Close();
+				serialPort.Dispose();
+				serialPort = null;
+			}
+		}
+		/// <summary>
+		/// 
+		/// </summary>
+		/// <param name="freq">周波数 31~16383の間で指定</param>
+		/// <returns></returns>
+		IEnumerable<byte> freqUpdate(int freq) {
+			return new byte[] { 0x81, (byte)((freq >> 7) & 0x7f), (byte)(freq & 0x7f) };
+		}
+		IEnumerable<byte> volUpdate(int vol) {
+			return new byte[] { 0x82, 0x0, (byte)((vol >> 1) & 0x7f) };
+		}
+		public void Update(int freq, int vol) {
+			var src = freqUpdate(freq).Concat(volUpdate(vol)).ToArray();
+			serialPort.Write(src, 0, src.Length);
+		}
+		public void UpdateFreq(int freq) {
+			var src = freqUpdate(freq).ToArray();
+			serialPort.Write(src, 0, src.Length);
+		}
+		public void UpdateVol(int vol) {
+			var src = volUpdate(vol).ToArray();
+			serialPort.Write(src, 0, src.Length);
+		}
+		public void Dispose() {
+			Close();
 		}
 	}
 }
